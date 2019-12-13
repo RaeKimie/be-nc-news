7a9db0cfbd50492324327d6415bd28f983f6a432
RaeKimie <nr.kim91@gmail.com>
2019-12-13 10:35:48 +0000

    added cors to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router");
+const cors = require("cors");
 const {
   handle500s,
   handle404s,
@@ -8,7 +9,7 @@ const {
   handle400s,
   handle422s
 } = require("./errors");
-
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 app.use("/*", handle404s);
