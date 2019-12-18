06e606e28c352743a1df18eae51766f7056127c4
RaeKimie <nr.kim91@gmail.com>
2019-12-18 16:20:30 +0000

    changed broken avatar url

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,32 +1,37 @@
 module.exports = [
   {
-    username: 'tickle122',
-    name: 'Tom Tickle',
-    avatar_url: 'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg',
+    username: "tickle122",
+    name: "Tom Tickle",
+    avatar_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiFHR-6ejeVWEcigsgozVGPphvMDALQUL2K8srEpBJjahzRqh50A&s"
   },
   {
-    username: 'grumpy19',
-    name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg',
+    username: "grumpy19",
+    name: "Paul Grump",
+    avatar_url: "https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg"
   },
   {
-    username: 'happyamy2016',
-    name: 'Amy Happy',
-    avatar_url: 'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    username: "happyamy2016",
+    name: "Amy Happy",
+    avatar_url:
+      "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729"
   },
   {
-    username: 'cooljmessy',
-    name: 'Peter Messy',
-    avatar_url: 'https://i.imgur.com/WfX0Neu.jpg',
+    username: "cooljmessy",
+    name: "Peter Messy",
+    avatar_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgkb-MX1kIhEohv-TtgZhDIJnoNkvxjg17vE3OPQZABjrD7KvJYw&s"
   },
   {
-    username: 'weegembump',
-    name: 'Gemma Bump',
-    avatar_url: 'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg',
+    username: "weegembump",
+    name: "Gemma Bump",
+    avatar_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkHTR1ilYNOLH4CR_bZGQBlmlODckPjTXvTaKsAo8HLSS_o_-d4A&s"
   },
   {
-    username: 'jessjelly',
-    name: 'Jess Jelly',
-    avatar_url: 'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg',
-  },
+    username: "jessjelly",
+    name: "Jess Jelly",
+    avatar_url:
+      "https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg"
+  }
 ];
